c9c1c9bdb54c1e18bb0e5c01173db8039df65ac4
Dylan McCarthy <Dylan.McCarthy@dpm.com.au>
2021-02-22 19:38:47 +1100

    Cleaning up unused code

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from numpy import true_divide
 import pika
 from pika.exchange_type import ExchangeType
-import json
+import jsons
 from Tracking import Tracking
+import redis
+import base64
 
 
 tracking = Tracking()
 
-def process_message(ch, method, properties, body):
-    sat_names = json.loads(body)
+def process_message(body):
+    sat_names = jsons.loads(body)
     tracking.get_pos(sat_names)
 
 def main():
-    print('connecting to mq')
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',credentials=pika.PlainCredentials('Worker','workerPassword')))
-    print('connected, getting channel')
-    channel = connection.channel()
-    print('got channel, delcaring exchange')
-    channel.exchange_declare('SatWorker',exchange_type=ExchangeType.topic,durable=True)
+    r = redis.Redis(host='localhost',port=6379, db=0)
+    r_channel = r.pubsub()
+    r_channel.subscribe('WorkIn')
+    r_channel.get_message()
 
-    channel.basic_consume('WorkIn',on_message_callback=process_message,auto_ack=True)
+    while(True):
+        message = r_channel.get_message()
+        if message:
+            if(message.get('type') != 'subscribe'):
+                data = bytearray(message.get('data'))
+                process_message(data.decode())
 
-    channel.start_consuming()
 
 if __name__ == '__main__':
     main()
